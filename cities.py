7b644379b1ecfa8fb0da4a48a347842788e1ca78
Alejandro Avila Carrion <thealexabi@gmail.com>
2022-11-09 18:50:34 +0000

    Added a constructor to City with type tests

--- cities.py
+++ cities.py
@@ -1,12 +1,36 @@
+# pylint: disable = C0114, C0115, C0116
+# pycodestyle: disable = E501
 from typing import Dict, List, Tuple
 
+
 class City:
-    ...
+    def __init__(self, name: str, country: str, num_attendees: int,
+                 lat: float, long: float):
+        if not isinstance(name, str):
+            raise TypeError("Name should be given as a string")
+        if not isinstance(country, str):
+            raise TypeError("Country should be given as a string")
+        if not isinstance(num_attendees, int):
+            raise TypeError("Number of attendees should be given as a integer")
+        if not isinstance(lat, float):
+            raise TypeError("Latitude should be given as a floating point "
+                            "number")
+        if not isinstance(long, float):
+            raise TypeError("Longitude should be given as a floating point "
+                            "number")
+
+        self.name = name
+        self.country = country
+        self.num_attendees = num_attendees
+        self.lat = lat
+        self.long = long
+
     def distance_to(self, other: 'City') -> float:
         raise NotImplementedError
 
     def co2_to(self, other: 'City') -> float:
         raise NotImplementedError
+
 
 class CityCollection:
     ...
@@ -37,4 +61,3 @@ class CityCollection:
 
     def plot_top_emitters(self, city: City, n: int, save: bool):
         raise NotImplementedError
-
