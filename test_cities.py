fdbd855dea913dd8b8b5e0c8abe0fb47334d106d
Alejandro Avila Carrion <thealexabi@gmail.com>
2022-11-10 23:34:05 +0000

    Added City value tests to test file

--- test_cities.py
+++ test_cities.py
@@ -27,3 +27,30 @@ def test_City_attributes_type():
         City('Paris', 'France', 173, 48.8566101, 2)
     assert exception.match("Longitude should be given as a floating point "
                            "number")
+    
+def test_City_attributes_value():
+
+    with raises(ValueError) as exception:
+        City('paris', 'France', 173, 48.8566101, 2.3514992)
+    assert str(exception.value) == ("First letter in name should be upper "
+                                    "case")
+
+    with raises(ValueError) as exception:
+        City('Paris', 'france', 173, 48.8566101, 2.3514992)
+    assert str(exception.value) == ("First letter in country should be upper "
+                                    "case")
+
+    with raises(ValueError) as exception:
+        City('Paris', 'France', -173, 48.8566101, 2.3514992)
+    assert str(exception.value) == ("Number of attendees should be a positive "
+                                    "integer")
+
+    with raises(ValueError) as exception:
+        City('Paris', 'France', 173, 91.0, 2.3514992)
+    assert str(exception.value) == ("Latitude should be between -90 and 90 "
+                                    "degrees")
+
+    with raises(ValueError) as exception:
+        City('Paris', 'France', 173, 48.8566101, -181.0)
+    assert str(exception.value) == ("Longitude should be between -180 and 180 "
+                                    "degrees")
