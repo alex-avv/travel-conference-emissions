16af60d446547257e2b0339a8a9fefef2b31e0bf
Alejandro Avila Carrion <thealexabi@gmail.com>
2022-11-10 23:38:21 +0000

    Updated assert code for City type tests

--- test_cities.py
+++ test_cities.py
@@ -8,26 +8,28 @@ def test_City_attributes_type():
 
     with raises(TypeError) as exception:
         City(9, 'France', 173, 48.8566101, 2.3514992)
-    assert exception.match("Name should be given as a string")
+    assert str(exception.value) == ("Name should be given as a string")
 
     with raises(TypeError) as exception:
         City('Paris', 7.0, 173, 48.8566101, 2.3514992)
-    assert exception.match("Country should be given as a string")
+    assert str(exception.value) == ("Country should be given as a string")
 
     with raises(TypeError) as exception:
         City('Paris', 'France', 173.0, 48.8566101, 2.3514992)
-    assert exception.match("Number of attendees should be given as an integer")
+    assert str(exception.value) == ("Number of attendees should be given as "
+                                    "an integer")
 
     with raises(TypeError) as exception:
         City('Paris', 'France', 173, '48.8566101', 2.3514992)
-    assert exception.match("Latitude should be given as a floating point "
-                           "number")
+    assert str(exception.value) == ("Latitude should be given as a floating "
+                                    "point number")
 
     with raises(TypeError) as exception:
         City('Paris', 'France', 173, 48.8566101, 2)
-    assert exception.match("Longitude should be given as a floating point "
-                           "number")
-    
+    assert str(exception.value) == ("Longitude should be given as a floating "
+                                    "point number")
+
+
 def test_City_attributes_value():
 
     with raises(ValueError) as exception:
