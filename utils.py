d2429a30e3c1512880fbaa43968b18ac0de7b84f
Alejandro Avila Carrion <thealexabi@gmail.com>
2022-11-12 14:39:44 +0000

    Added type tests for city and country data

--- utils.py
+++ utils.py
@@ -29,6 +29,7 @@ def read_attendees_file(filepath: Path) -> CityCollection:
                            for row in attendees_raw_data[1:]]
 
     # Changing columns to appropriate data types for the City object's input
+    # and carrying out some additional type tests on the instances
     cits, countrs, ns, lats, longs = list(map(list, zip(*cities_ordered_data)))
     cits, countrs, ns, lats, longs = assign_cities_data_types(cits, countrs,
                                                               ns, lats, longs)
@@ -40,4 +41,22 @@ def assign_cities_data_types(cits: List[str], countrs: List[str],
                              ns: List[str], lats: List[str], longs: List[str]
                              ) -> Tuple[List[str], List[str], List[int],
                                         List[float], List[float]]:
+    numbers = [str(num) for num in range(10)]
+
+    for i, _ in enumerate(cits):
+        try:
+            if (any([num in cits[i] for num in numbers])
+                and cits[i] != ('Skrzatow '
+                                '1')):
+                raise TypeError('String contains numbers')
+        except Exception as err:
+            raise TypeError(f"City '{cits[i]}' in row {i + 2} should be "
+                            "written as a string") from err
+        try:
+            if any([num in countrs[i] for num in numbers]):
+                raise TypeError('String contains numbers')
+        except Exception as err:
+            raise TypeError(f"Country '{countrs[i]}' in row {i + 2} should be "
+                            "written as a string") from err
+
     raise NotImplementedError
