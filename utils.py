7af0b5b1349e9acceac832fb5a7238463548cfcb
Alejandro Avila Carrion <thealexabi@gmail.com>
2022-11-12 14:12:39 +0000

    Added code to import attendees csv data

--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
 # pylint: disable = C0114, C0116
+from pathlib import Path
+import csv
 from cities import City, CityCollection
 
 
 def read_attendees_file(filepath: Path) -> CityCollection:
+    if not isinstance(filepath, Path) and not isinstance(filepath, str):
+        raise TypeError("File path should be given as a pathlib.Path object "
+                        "or a string")
+
+    with open(filepath, 'r', encoding="utf-8") as attendees_file:
+        csv_reader = csv.reader(attendees_file)
+        attendees_raw_data = [record for record in csv_reader]
+
     raise NotImplementedError
