b654ecf325a63ed2b4b2a855d49783a1f0c76495
Alejandro Avila Carrion <thealexabi@gmail.com>
2022-11-12 19:31:33 +0000

    Added parametrisation to City value tests (json)

--- test_cities.py
+++ test_cities.py
@@ -19,29 +19,12 @@ def test_City_properties_type(test_name):
     assert str(exception.value) == expected_err_message
 
 
-def test_City_properties_value():
+@mark.parametrize('test_name', dict_test_fixtures['City_properties_value'])
+def test_City_properties_value(test_name):
+    buffer = list(test_name.values())[0]
+    city_input = buffer['properties']
+    expected_err_message = buffer['expected_err_message']
 
     with raises(ValueError) as exception:
-        City('paris', 'France', 173, 48.8566101, 2.3514992)
-    assert str(exception.value) == ("First letter in name should be upper "
-                                    "case")
-
-    with raises(ValueError) as exception:
-        City('Paris', 'france', 173, 48.8566101, 2.3514992)
-    assert str(exception.value) == ("First letter in country should be upper "
-                                    "case")
-
-    with raises(ValueError) as exception:
-        City('Paris', 'France', -173, 48.8566101, 2.3514992)
-    assert str(exception.value) == ("Number of attendees should be a positive "
-                                    "integer")
-
-    with raises(ValueError) as exception:
-        City('Paris', 'France', 173, 91.0, 2.3514992)
-    assert str(exception.value) == ("Latitude should be between -90 and 90 "
-                                    "degrees")
-
-    with raises(ValueError) as exception:
-        City('Paris', 'France', 173, 48.8566101, -180.0)
-    assert str(exception.value) == ("Longitude should be between -180 and 180 "
-                                    "degrees")
+        City(*city_input)
+    assert str(exception.value) == expected_err_message
