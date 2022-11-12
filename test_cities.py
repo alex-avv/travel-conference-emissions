c22b7a02504629ba8642729151ac80aeab22c2f9
Alejandro Avila Carrion <thealexabi@gmail.com>
2022-11-12 21:23:11 +0000

    Added tests for City distance_to and co2_to methods

--- test_cities.py
+++ test_cities.py
@@ -28,3 +28,27 @@ def test_City_properties_value(test_name):
     with raises(ValueError) as exception:
         City(*city_input)
     assert str(exception.value) == expected_err_message
+
+
+@mark.parametrize('test_name', dict_test_fixtures['distance_to'])
+def test_distance_to(test_name):
+    buffer = list(test_name.values())[0]
+    host_city_input = buffer['properties_host_city']
+    visitor_city_input = buffer['properties_visitor_city']
+    expected_value = buffer['expected_value']
+
+    host_city, visitor_city = City(*host_city_input), City(*visitor_city_input)
+    distance = visitor_city.distance_to(host_city)
+    assert distance == expected_value
+
+
+@mark.parametrize('test_name', dict_test_fixtures['co2_to'])
+def test_co2_to(test_name):
+    buffer = list(test_name.values())[0]
+    host_city_input = buffer['properties_host_city']
+    visitor_city_input = buffer['properties_visitor_city']
+    expected_value = buffer['expected_value']
+
+    host_city, visitor_city = City(*host_city_input), City(*visitor_city_input)
+    co2 = visitor_city.co2_to(host_city)
+    assert co2 == expected_value
