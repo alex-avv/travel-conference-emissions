cd6695924d9300d3ba3266e7125b8245130c656a
Alejandro Avila Carrion <thealexabi@gmail.com>
2022-11-12 14:19:47 +0000

    Added code to correctly arrange data columns

--- utils.py
+++ utils.py
@@ -13,4 +13,18 @@ def read_attendees_file(filepath: Path) -> CityCollection:
         csv_reader = csv.reader(attendees_file)
         attendees_raw_data = [record for record in csv_reader]
 
+    # Checking the order in which the columns are arranged in the attendees
+    # raw data
+    ordered_fieldnames = [('city', str), ('country', str), ('N', int),
+                          ('lat', float), ('lon', float)]
+    ordered_col_indexes = [attendees_raw_data[0].index(fieldname)
+                           for required_fieldname, _ in ordered_fieldnames
+                           for fieldname in attendees_raw_data[0]
+                           if fieldname == required_fieldname]
+
+    # Rearranging the columns in the attendees raw data so they match the order
+    # expected in the City object's input
+    cities_ordered_data = [tuple([row[i] for i in ordered_col_indexes])
+                           for row in attendees_raw_data[1:]]
+
     raise NotImplementedError
